0dbd0d1dae372ced03a0f3c83f1b5a7a40885903
Mahmudul107 <mahmudulislam378@gmail.com>
2023-05-19 17:00:18 +0600

    server is running on port completed

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Vroom Vroom Toys is running');
+})
+
+app.listen(port, () =>{
+    console.log(`Vroom Vroom is running on ${port}`);
+})
